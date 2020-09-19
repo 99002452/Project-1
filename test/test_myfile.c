2440c4332d136489fe7d4cb0b1c8da017699050d
99002452 <71481286+99002452@users.noreply.github.com>
2020-09-19 14:21:59 +0530

    Update test_myfile.c

--- test/test_myfile.c
+++ test/test_myfile.c
@@ -3,7 +3,7 @@
 
 /* Modify these two lines according to the project */
 #include <myfile.h>
-#define PROJECT_NAME    "DisplayOP"
+#define PROJECT_NAME    "CALENDAR AND REMINDER"
 
 /* Prototypes for all the test functions */
 void test_printop(void);
@@ -16,14 +16,32 @@ int main() {
     return CU_get_error();
   CU_pSuite suite = CU_add_suite(PROJECT_NAME, 0, 0);
 /* Note: Do not edit END */
-  
-  
-  /* Add your test functions in this format for testing*/
+    /* Add your test functions in this format for testing*/
 
   CU_get_day(dd,mm,yyyy);
+
+ int check_leapYear(int year)
+ { //checks whether the year passed is leap year or not
+    if(year % 400 == 0 || (year % 100!=0 && year % 4 ==0))
+       return 1;
+    return 0;
+}
 	
-
-
+int checkNote(int dd, int mm){
+    FILE *fp;
+    fp = fopen("note.dat","rb");
+    if(fp == NULL){
+        printf("Error in Opening the file");
+    }
+    while(fread(&R,sizeof(R),1,fp) == 1){
+        if(R.dd == dd && R.mm == mm){
+            fclose(fp);
+            return 1;
+        }
+    }
+    fclose(fp);
+    return 0;
+}
 /* Note: Do not edit START*/
   /* Setup Test Framework to output the result to Screen */
   CU_basic_set_mode(CU_BRM_VERBOSE);
